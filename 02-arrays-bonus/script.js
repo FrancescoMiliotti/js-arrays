6b43937e50db7a0a94ecc5cedf1b1bb32bd7da49
francesco.miliotti <francesco.miliotti@email.com>
2025-11-04 19:51:08 +0100

    correzione punto 2 e 5 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,8 +15,18 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = new Array ("Nathan", "Fabio", "Carlo", "Lewis");
-console.log(longNames);
+const longNames = []
+
+for (let i = 0; i < teachers.length; i++) {
+  const teacher = teachers[i];
+
+  if (teacher.length >= 5 ) {
+    longNames.push(teacher);
+  }
+  
+}
+
+console.log("Long Name", longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(5, 1);
@@ -31,5 +41,5 @@ console.log("is Fabio present?", isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.toString();
+const teachersString = teachers.join( );
 console.log(teachersString);
